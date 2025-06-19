c3dc691e3548f6ce4153911d6b9155bdadc1eba6
Sonu111kumar <kumarsonu210802@gmail.com>
2025-06-19 07:58:42 +0530

    Update Middleware and layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs';
-import Provider from './Provider';
+import Provider from "./provider";
 import { Toaster } from "sonner";
 const geistSans = Geist({
   variable: "--font-geist-sans",
